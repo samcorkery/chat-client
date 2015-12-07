916a6c4209904368e4a29958f46c6acd167d6a3c
Carson Woods <cwoods289@gmail.com>
2015-12-07 11:23:57 -0600

    Finished Server Application and Edited Client Application

--- client/Client.java
+++ client/Client.java
@@ -3,18 +3,27 @@ import java.net.*;
 import java.util.Scanner;
 
 class Client{
-  public static void main(String args[]) {
+  public static void main(String args[]) throws IOException {
     
+
+    if (args.length != 2) {
+        System.err.println("Usage: java Client <host name> <port number>");
+            System.exit(1);
+        }
+
     //gets host name and port
     String host = args[0];
     int port = Integer.parseInt(args[1]);
     
+    
+    try (
     //creates socket 
     Socket messageSocket = new Socket(host, port);
     
     //Creates PrintWriter, BufferedReader objects, and Scanner Objects
     PrintWriter output = new PrintWriter(messageSocket.getOutputStream(), true);
     BufferedReader input = new BufferedReader(new InputStreamReader(messageSocket.getInputStream()));
+    ) {
     Scanner contentFromServer = new Scanner(input);
     Scanner contentFromUser = new Scanner(System.in);
 
@@ -37,5 +46,13 @@ class Client{
             System.out.println(clientMessage);
         }      
     }
-  }
+    } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + host);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                host);
+            System.exit(1);
+        }        
+    }
 }
--- server/Server.java
+++ server/Server.java
@@ -3,26 +3,39 @@ import java.io.*;
 import java.util.Scanner;	
 
 class Server {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		
+		if (args.length != 1) {
+            System.err.println("Usage: java Server <port number>");
+            System.exit(1);
+        }
+
 		//get user port
 		int port = Integer.parseInt(args[0]);
 
+		try (
 		//Build server side socket
 		ServerSocket serverSocket = new ServerSocket(port);
 		Socket client = serverSocket.accept();
 
-		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
+		PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        ) {
 		//create input and output variables
 		String input, output;
 
 		//creates new scanner object
 		Scanner content = new Scanner(in);
+		input = content.nextLine();
 
-		while ((inputLine = in.readLine()) != null) {
-                outputLine = kkp.processInput(inputLine);
-                out.println(outputLine);
+		//manages output
+		while (input != null) {
+           output = input;
+           out.println(output);
+		}
+		} catch (IOException e) {
+			System.out.println("Exception caught when trying to listen on port " + port + " or listening for a connection");
+            System.out.println(e.getMessage());
+		}
 	}
 }
